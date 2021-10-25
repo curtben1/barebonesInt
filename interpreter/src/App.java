9682f715d3e1d0056ae9aedb9f79fd01f9246bb5
Ben <curtben1@hotmail.co.uk>
2021-10-25 09:57:49 +0100

    commenting and formatting

--- interpreter/src/App.java
+++ interpreter/src/App.java
@@ -14,19 +14,15 @@ import java.util.HashMap;
 public class App {
     static HashMap<String,Integer>  variables = new HashMap<String,Integer>();
     public static void main(String[] args) throws Exception {
-        
         String[][] codeArr = generateCode();
         execute(codeArr);
-        
-
-
     }
     
     public static String[][] generateCode() throws Exception{
         /**
         Function that effectively performs lexical analysis by generating a token stream and removing 
-        unnecesary characters and generating a token stream/array of instructions. It does not check for
-        errors at this stage as it is interpreting not compiling so can at runtime with no issues.
+        unnecesary characters. It does not check for errors at this stage as it is interpreting not compiling
+        so can at runtime with no issues.
         **/
         System.out.println("Enter file name/path to file:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
@@ -49,7 +45,13 @@ public class App {
 
 
     public static void execute(String[][] codeArr) throws Exception{
-        
+        /** 
+        Method to read and execute the perviously formed code Array
+        uses a for loop to iterate over each "line" and a switch case structure
+        to check each instruction and a hash map to store variables. Becomes recursive
+        if there is a while loop in the bare bones code. This does mean the memory
+        performance of the while statement is incorrect but should not matter too much
+        */
         int newVal;
         int lineNum = 0;
         for (int i=0;i< codeArr.length;i++){
@@ -71,15 +73,18 @@ public class App {
                     System.out.println(line[1] +": " + Integer.toString(newVal));
                     break;
                 case "while":
+                    /*
+                    code to execute a while loop,  recursively calls execute. Uses a counter of the 
+                    number of while and end commands to establish nesting level and when to end the
+                    while. The tempLineNum variable is used to establish where to jump to once the 
+                    loop is executed and also to establish the length of the while loop.
+                    */
                     if (! (line[2].equals("not") || line[3].equals("0") ||line[3].equals("do"))){
                         System.out.println("error bad syntax");
                     }
                     else{
-                        //while loop time
                         int tempLineNum = lineNum;
                         while (variables.get(line[1]) !=0){
-
-                            //System.out.println("the loop condition is: " + line[1] + Integer.toString(variables.get(line[1])));
                             tempLineNum = lineNum;
                             int endCounter = 0;
                             do{
@@ -91,14 +96,10 @@ public class App {
                                     if (endCounter != 0){
                                         tempLineNum ++;
                                     }
-                                    //System.out.println("end counter"+ Integer.toString(endCounter));
-                                    
                                 }
                                 else{
                                     tempLineNum ++;
-                                }
-                                
-                                
+                                }  
                             }while (endCounter !=0);
                             String[][] tempArr = new String[tempLineNum-lineNum][1];
                             System.arraycopy(codeArr, lineNum + 1,tempArr, 0, tempLineNum-lineNum);
@@ -106,14 +107,12 @@ public class App {
                         }
                         i=tempLineNum;
                         lineNum=tempLineNum;
-                        //System.out.println("jump to "+ Arrays.toString(codeArr[lineNum]));
                     }
                     break;
                 case "end":
                     break;
                 default:
-                    System.out.println("unrecognised instruction");
-                    
+                    System.out.println("unrecognised instruction");  
             } 
             lineNum++;  
         }
